79c71547d4c0700b9dfe01e2a65b18bde3ba14a2
DrItchy <thomas.simard@polymtl.ca>
2022-09-13 18:15:13 -0400

    1.2

--- exercice.py
+++ exercice.py
@@ -16,7 +16,7 @@ def remove_third_char(string: str) -> str:
         return string
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    pass
+    return string.replace(old_char, new_char)
 
 
 def get_number_of_char(string: str, char: str) -> int:
