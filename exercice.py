f7e7d2e84ef43933c018adfc8e2399dcdf23482e
DrItchy <thomas.simard@polymtl.ca>
2022-09-13 18:25:39 -0400

    1.5

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ def is_even_len(string: str) -> bool:
 
 def remove_third_char(string: str) -> str:
     if len(string)>=3:
-        return string[0:1]+string[3:]
+        return string[0:2]+string[3:]
     else:
         return string
 
